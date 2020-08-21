5d2de17ed7b95dc7018ee9a364634047782f1dd7
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-21 14:53:16 +0100

    Add reducer function with ADD_ROCKET case

---
+++ src/reducers/reducer.js
@@ -0,0 +1,16 @@
+const defaultState = {
+  rockets = []
+}
+
+reduce = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_ROCKET': 
+    return {
+      ...state,
+      rockets: action.payload
+    }
+    default: return state
+  }
+}
+
+export default reducer
